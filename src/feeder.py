e1356f5334d23f59ed00f0f84e12128f85533b8e
Trop-TTA <trottalukas@gmail.com>
2017-02-22 17:46:03 +0100

    extractfeatures ready. feeder in process

--- src/feeder.py
+++ src/feeder.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 class Feeder:
-    def __init__(self, featurespath, labelspath=None, examplesratio=0.95, batchsize=None):
+    def __init__(self, featurespath, labelspath=None, opts={}):
+
+        opts.update({'examplesratio': 0.95, })
+        self.opts = opts
+
         if labelspath is None:
             from extractfeatures import FeaturesExtractor as fe
             featurespath, labelspath = fe.getdatapaths(featurespath)
@@ -10,8 +14,18 @@ class Feeder:
 
         print('loading features')
         self.features = np.loadtxt(featurespath)
+        if 'featuremutation' in opts:
+            tmpres = [None] * self.features.shape[0]
+            ressize = 0
+            for i in range(self.features.shape[0]):
+                tmpres[i] = opts['featuremutation'](self.features[i])
+                ressize += tmpres[i].shape[0]
+
+            self.features = opts['featuremutation'](self.features)
         print('loading labels')
         self.labels = np.loadtxt(labelspath)
+        if 'labelmutation' in opts:
+            self.labels = opts['labelmutation'](self.labels)
 
         assert self.features.shape[0] == self.labels.shape[0]
 
@@ -21,9 +35,12 @@ class Feeder:
         if len(self.labels.shape) == 1:
             self.labels = np.array([[nb] for nb in self.labels])
 
-        self.batchsize = batchsize
+        if 'batchsize' in opts:
+            self.batchsize = opts['batchsize']
+        else:
+            self.batchsize = None
         self.nbsamples = self.features.shape[0]
-        self.nbexamples = int(np.ceil(self.nbsamples * examplesratio))
+        self.nbexamples = int(np.ceil(self.nbsamples * opts['examplesratio']))
         self.nbtests = self.nbsamples - self.nbexamples
 
         #random permutation of the samples in way to have an otpimal learning
@@ -76,3 +93,9 @@ class Feeder:
 
     def __iter__(self):
         return self
+
+
+class AudioFeeder(Feeder):
+    def __init__(self, featurespath, labelspath=None, opts={}):
+        import extractfeatures as ef
+        opts.update({'featuremutation': ef.ExtractMonoAudioFiles.featuremutation, 'labelmutation': ef.ExtractMonoAudioFiles.labelmutation})
