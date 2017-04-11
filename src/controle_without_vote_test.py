4ca57ce0c4af5c28b6a7cac7d715ca0010bd6ab0
gabmis <gabriel.misrachi@gmail.com>
2017-04-11 17:23:51 +0200

    latest session code in controle_without_vote

--- src/controle_without_vote_test.py
+++ src/controle_without_vote_test.py
@@ -53,7 +53,7 @@ y_ = tf.placeholder(tf.float32, [None, nblabels])
 #cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 cross_entropy = tf.reduce_mean(tf.reduce_sum(tf.pow(y-y_,2)))
 
-train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
 
@@ -61,41 +61,33 @@ correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-tours = 100
+tours = 1
 
-entropy_graph = np.zeros(tours * exfeeder.nbsamples // emaf.batchsize)
-accuracy_graph = np.zeros(tours * exfeeder.nbsamples // emaf.batchsize)
-time = np.zeros(tours * exfeeder.nbsamples // emaf.batchsize)
+entropy_graph = np.zeros(1000)
+accuracy_graph = np.zeros(1000)
+time = np.zeros(1000)
 
 sess.run(init)
 
 
-
-for i in range(tours * exfeeder.nbsamples // emaf.batchsize):
+#tours * exfeeder.nbsamples // emaf.batchsize
+for i in range(1000):
     print("batch %d/%d" % (i + 1, tours * exfeeder.nbsamples // emaf.batchsize))
     time[i] = i
     # on effectue une étape de l'entrainement (c'est a dire un gradient descent sur tout le batch)
     batch_xs, batch_ys = next_batch()
     #print(sess.run(y, feed_dict={x: batch_xs, y_: batch_ys}))
-    ent1, accu1 = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys}), sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys})
+    ent1, accu1 = sess.run([cross_entropy, accuracy], feed_dict={x: batch_xs, y_: batch_ys})
     print("entropy1  "+ str(ent1))
     print("accuracy1  " + str(accu1))
     accuracy_graph[i] = accu1
-    print(accuracy_graph)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    ent2, accu2 = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys}), sess.run(accuracy,feed_dict={x: batch_xs, y_: batch_ys})
+    ent2, accu2 = sess.run([cross_entropy, accuracy],feed_dict={x: batch_xs, y_: batch_ys})
     print("entropy2  " + str(ent2))
     print("accuracy2  "+str(accu2))
     entropy_graph[i] = ent2
 
-    #for gv in grads_and_vars:
-     #   print(str(sess.run(gv[0])) + " - " + gv[1].name, feed_dict={x: batch_xs, y_: batch_ys})
 
-plt.figure(1)
-plt.plot(time, entropy_graph)
-plt.figure(2)
-plt.plot(time, accuracy_graph)
-plt.show()
 
 #training_en_reg_lin.exfeeder.switchmode()
 exfeeder.switchmode()
@@ -103,3 +95,8 @@ x_control_base,y_control_base = exfeeder.getbatch(batchsize=exfeeder.nbtests)
 
 print(sess.run(accuracy, feed_dict={x: x_control_base, y_: y_control_base}))
 
+plt.figure(1)
+plt.plot(time, entropy_graph)
+plt.figure(2)
+plt.plot(time, accuracy_graph)
+plt.show()
