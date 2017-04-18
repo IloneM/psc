855e067e6f1e581b1b5dfdb6ae99a626aa27484c
Trop-TTA <trottalukas@gmail.com>
2017-04-19 01:50:56 +0200

    My changes for mysql (with the files)

---
+++ src/extractfeaturesmysql.py
@@ -0,0 +1,165 @@
+from os import listdir,remove
+from os.path import join,isfile
+from mysqlstuffs import Database
+from six import iteritems
+import librosa.feature as lrft
+import librosa.core as lrco
+import numpy as np
+import sys
+
+def absolutepromt(prompt="Enter y/n?",choices=["Y","y","n","N"],yeschoices=['Y','y'],error="Invalid choice"):
+    while True:
+        result = input(prompt)
+        if result in choices:
+            return result in yeschoices
+        print(error)
+
+class ExtractMonoAudioFiles:
+    #static vars to be manually set
+    ##the natural byterate of the examples here but we can consider modifying it
+    sr = 44100
+    nblabels = 88
+    batchsize = 1000
+    #featurefunc = lambda y, sr: lrft.mfcc(y, sr, n_mfcc=1).T
+    #featurefunc = lambda *x: x
+    #featurefunc = lambda y, sr: lrft.melspectrogram(y, sr).T
+    featurefunc = lrft.melspectrogram
+    tablecontext = 'melspectrogram'
+    inpath = '../simple-wdir'
+    outdb = 'psc'
+    
+    ## for feeder
+    featuremutation = lambda y, sr: lrft.melspectrogram(y, sr).T
+
+#    @staticmethod
+#    def labelmutation(pitch):
+#        labelvect = np.zeros(shape=(1, ExtractMonoAudioFiles.nblabels))
+#        labelvect[:, int(pitch)] = np.ones(nbsamples)
+#        return labelvect
+    ##
+
+    def __init__(self, inpath=None, outdb=None):
+        if inpath is None:
+            self.inpath = ExtractMonoAudioFiles.inpath
+        else:
+            self.inpath = inpath
+        if outdb is None:
+            self.outdb = ExtractMonoAudioFiles.outdb
+        else:
+            self.outdb = outdb
+        #self.tablecontext = self.featurefunc.__name__
+#        if featurefunc is None:
+#            self.featurefunc = ExtractMonoAudioFiles.featurefunc
+#        else:
+#            self.featurefunc = featurefunc
+
+    def __call__(self, inpath=None, outdb=None):
+        if inpath is None:
+            inpath = self.inpath
+        if outdb is None:
+            outdb = self.outdb
+        self.extract(inpath, outdb)
+
+    def extract(self, inpath, outdb):
+        files = [f for f in listdir(inpath) if f.endswith('.txt')]
+
+        nbfiles = len(files)
+        if nbfiles <= 0:
+            raise ValueError('you must provide a least one file.')
+
+        loopit = 1
+        for smd in self.__extractmetadata(inpath, files):
+            self.__storedata(outdb, self.__computedata(inpath, smd), loopit)
+
+            if loopit in [1, nbfiles] or not loopit % 100:
+                print('loading file %d/%d' % (loopit, nbfiles))
+            loopit += 1
+
+    def __extractmetadata(self, path, filelist):
+        samplesmetadata = []
+
+        for f in filelist:
+            with open(join(path, f), 'r') as fs:
+                #get the the onset and offset plus midi pitch
+                onset, offset, midipitch = tuple(map(float, fs.readlines()[1][:-1].split('\t')))
+            #first is the midi pitch rescaled into [0,87] range
+            #second is a tuple of wav path, onset and duration
+            samplesmetadata.append(((f[:-3] + 'wav', onset, offset-onset), int(midipitch - 21)))
+        return samplesmetadata
+
+    def __computedata(self, path, samplemetadata):
+        meta, pitch = samplemetadata
+
+        audiodat = lrco.load(join(path, meta[0]), sr=self.sr,
+                             offset=meta[1], duration=meta[2])
+        audiodat = ExtractMonoAudioFiles.featurefunc(*audiodat).T
+
+#        pitchvect = np.array([pitch] * audiodat.shape[0])
+
+        #return (audiodat, pitch)
+        return {'features': audiodat, 'label': pitch}
+
+    def __storedata(self, outdb, data, sampleid):
+        pass
+#        assert len(outpaths) == len(data) and len(data) == 2
+#        datasize = len(data)
+        db = Database(outdb)
+        #TODO: check if glue corresponds to new set of tables and alter it if not and check every table so and etc
+        #create table if not exists
+#        if not db.istable('glue'):
+#            glue = {'id': 'INT(11) NOT NULL AUTO_INCREMENT'}
+#            for k,v in iteritems(data):
+#                tmpstruct = {'id': 'INT(11) NOT NULL AUTO_INCREMENT'}
+#                if len(v.shape) > 1:
+#                    nbfeat = v.shape[1]
+#                else:
+#                    nbfeat = 1
+#                for i in range(nbfeat):
+#                    tmpstruct['item_' + str(i+1)] = 'FLOAT DEFAULT NULL'
+#                tmpstruct['PRIMARY KEY'] = '(id)'
+#                db.createtable(k, tmpstruct)
+#                glue[k+'_id'] = 'INT(11) DEFAULT NULL'
+#
+#            glue['PRIMARY KEY'] = '(id)'
+#            db.createtable('glue', glue)
+        features = data['features']
+        label = data['label']
+
+        if not db.istable(self.tablecontext):
+            tmpstruct = {'id': 'INT(11) NOT NULL AUTO_INCREMENT',
+                         'label_id': 'INT(11) DEFAULT NULL',
+                         'sample_id': 'INT(11) DEFAULT NULL'}
+            if features.ndim > 1:
+                nbfeat = features.shape[1]
+            else:
+                nbfeat = 1
+
+            for i in range(nbfeat):
+                tmpstruct['feature_' + str(i+1)] = 'FLOAT DEFAULT NULL'
+            tmpstruct['PRIMARY KEY'] = '(id)'
+            db.createtable(self.tablecontext, tmpstruct)
+
+        #fill table
+        rowsnb = features.shape[0]
+        if features.ndim > 1:
+            colsnb = features.shape[1]
+        else:
+            colsnb = 1
+
+        formateddatas = []
+        for row in range(rowsnb):
+            formatedrow = {'label_id': label, 'sample_id': sampleid}
+            if colsnb > 1:
+                for col in range(colsnb):
+                    formatedrow['feature_'+str(col+1)] = features[row,col]
+            else:
+                formatedrow['feature_1'] = features[row]
+            formateddatas.append(formatedrow)
+        db.insert(self.tablecontext, formateddatas)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        ex = ExtractMonoAudioFiles(*tuple(sys.argv[1:]))
+    else:
+        ex = ExtractMonoAudioFiles()
+    ex()
---
+++ src/feedermysql.py
@@ -0,0 +1,149 @@
+import numpy as np
+from extractfeaturesmysql import ExtractMonoAudioFiles as emaf
+from mysqlstuffs import Database
+#from abc import ABCMeta
+from abc import *
+
+#TODO: translate ids by rand permut
+
+class Feeder(metaclass=ABCMeta):
+    def __init__(self, opts={}):
+        opts.update({'examplesratio': 0.95, 'dbname': emaf.outdb})
+        self.opts = opts
+
+        self.db = Database(self.opts['dbname'])
+
+#        if labelspath is None:
+#            from extractfeatures import FeaturesExtractor as fe
+#            featurespath, labelspath = fe.getdatapaths(featurespath)
+#        self.labelspath = labelspath
+#        self.featurespath = featurespath
+
+#        if len(self.features.shape) == 1:
+#            self.features = np.array([[nb] for nb in self.features])
+#        if len(self.labels.shape) == 1:
+#            self.labels = np.array([[nb] for nb in self.labels])
+
+        if 'batchsize' in opts:
+            self.batchsize = opts['batchsize']
+        else:
+            self.batchsize = None
+        self.nbsamples = self.countsamples()
+        #self.nbsamples = self.db.count(emaf.tablecontext)
+        self.nbexamples = self.countexamples()
+        self.nbtests = self.nbsamples - self.nbexamples
+
+        self.examplemode = True
+
+    def getbatch(self, batchfeatures=None, batchlabels=None, batchsize=None):
+        if batchsize is None:
+            batchsize = self.batchsize
+        if batchsize is None:
+            raise ValueError("You must provide a valid batchsize.")
+        if self.examplemode:
+            if batchsize > self.nbexamples:
+                raise IndexError
+            choice = np.random.choice(self.nbexamples, batchsize, False)
+            return self.choiceintosamples(choice)
+        else:
+            return self.controlprocess(batchsize)
+
+    def switchmode(self):
+        self.examplemode = not self.examplemode
+
+    def __call__(self, batchfeatures=None, batchlabels=None, batchsize=None):
+        return self.getbatch(batchfeatures, batchlabels, batchsize)
+
+    def __next__(self):
+        return self.getbatch(batchsize=1)
+#        if self.examplemode:
+#            choice = np.random.randint(self.nbexamples)
+#        else:
+#            choice = np.random.randint(self.nbexamples, self.nbsamples)
+#        return (self.features[choice], self.labels[choice])
+
+    def __iter__(self):
+        return self
+
+    def controlprocess(self, batchsize):
+        if batchsize > self.nbtests:
+            raise IndexError
+        choice = np.random.choice(np.arange(self.nbexamples, self.nbsamples), batchsize, False)
+        return self.choiceintosamples(choice)
+
+    @abstractmethod
+    def countsamples(self):
+        pass
+
+    @abstractmethod
+    def choiceintosamples(self, choice):
+        pass
+
+    def countexamples(self):
+        return int(np.ceil(self.nbsamples * self.opts['examplesratio']))
+
+class AudioFeeder(Feeder):
+    def __init__(self, opts={}):
+#        import extractfeatures as ef
+        #opts.update({'featuremutation': ef.ExtractMonoAudioFiles.featuremutation, 'labelmutation': ef.ExtractMonoAudioFiles.labelmutation})
+        #opts.update({'featuremutation': ef.ExtractMonoAudioFiles.featurefunc, 'labelmutation': ef.ExtractMonoAudioFiles.labelmutation})
+        #opts.update({'labelmutation': ef.ExtractMonoAudioFiles.labelmutation})
+
+        self.nbfeatures = None
+        self.nblabels = emaf.nblabels
+
+        super().__init__(opts)
+
+        #nbfeatures definition
+        self.__next__()
+
+    def countsamples(self):
+        return self.db.count(emaf.tablecontext)
+
+    def choiceintosamples(self, choice):
+#        assert choice.ndim == 1
+#        assert choice
+        nbsamples = choice.size
+        nblabels = self.nblabels
+        tmpres = self.db.get(emaf.tablecontext, choice)
+
+        if self.nbfeatures is None:
+            self.nbfeatures = len(tmpres[0]) - 3
+        nbfeatures = self.nbfeatures
+
+        features = np.zeros(shape=(nbsamples, nbfeatures))
+        labels = np.zeros(shape=(nbsamples, nblabels))
+
+        try:
+            for i in range(nbsamples):
+                line = list(tmpres[i])
+                features[i] = line[3:]
+                labels[i, line[1]] = 1.
+        except IndexError:
+            if tmpres is not None:
+                nbreturned = len(tmpres)
+            else:
+                nbreturned = 0
+            print("%d/%d returned" % (nbreturned, choice.size))
+        return (features, labels)
+
+class AudioFeederContext(AudioFeeder):
+    def countexamples(self):
+        approxres = super().countexamples()
+        itempersample = self.itempersample = self.db.count(emaf.tablecontext, 'sample_id')
+        totitems = 0
+        itsample = 0
+        while totitems < approxres:
+            totitems += itempersample[itsample][0]
+            itsample += 1
+
+        self.examplefirstitem = itsample
+        return totitems
+
+    def controlprocess(self, batchsize):
+        res = []
+        nbgroupedsamples = len(self.itempersample)
+        for sampleit in range(self.examplefirstitem, nbgroupedsamples):
+            ids = np.array([idt[0] for idt in self.db.get(emaf.tablecontext, sampleit, ['id'], 'sample_id')])
+            res.append(self.choiceintosamples(ids))
+        return res
